875af95aeda9d6015103de4b1a8b625d7c2feb29
Joursoir <chat@joursoir.net>
2024-10-05 01:41:11 +0300

    add Ethernet packet parsing

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,73 @@
 #![no_main]
 
 use log::{error, warn, info};
+use core::convert::TryInto;
+use core::fmt;
 use uefi::boot::{self, SearchType};
 use uefi::prelude::*;
 use uefi::proto::network::snp::{SimpleNetwork, NetworkMode, NetworkState};
 use uefi::{Identify, Result};
+
+// Ethernet protocol type definitions.
+#[derive(Debug, Clone, Copy)]
+pub enum EtherProtoType {
+    ARP,
+    IPv4,
+    IPv6,
+    Unknown(u16),
+}
+
+impl EtherProtoType {
+    pub fn from_u16(value: u16) -> Self {
+        match value {
+            0x0806 => EtherProtoType::ARP,
+            0x0800 => EtherProtoType::IPv4,
+            0x86DD => EtherProtoType::IPv6,
+            other => EtherProtoType::Unknown(other),
+        }
+    }
+}
+
+impl fmt::Display for EtherProtoType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let protocol_str = match self {
+            EtherProtoType::ARP => "ARP",
+            EtherProtoType::IPv4 => "IPv4",
+            EtherProtoType::IPv6 => "IPv6",
+            EtherProtoType::Unknown(value) => return write!(f, "Unknown(0x{:04X})", value),
+        };
+        write!(f, "{}", protocol_str)
+    }
+}
+
+pub const NET_ETHER_ADDR_LEN: usize = 6;
+pub const NET_ETHER_HEAD_SIZE: usize = 14;
+
+// Ethernet frame header definition.
+#[repr(C, packed)]
+#[derive(Debug)]
+pub struct EthernetHeader {
+    pub dst_mac: [u8; NET_ETHER_ADDR_LEN],
+    pub src_mac: [u8; NET_ETHER_ADDR_LEN],
+    pub ether_type: EtherProtoType,
+}
+
+impl EthernetHeader {
+    pub fn parse(packet: &[u8]) -> Result<Self> {
+        assert_eq!(packet.len(), NET_ETHER_HEAD_SIZE);
+
+        let dst_mac = packet[0..NET_ETHER_ADDR_LEN].try_into().unwrap();
+        let src_mac = packet[NET_ETHER_ADDR_LEN..2 * NET_ETHER_ADDR_LEN].try_into().unwrap();
+        let ether_type_raw = u16::from_be_bytes([packet[12], packet[13]]);
+        let ether_type = EtherProtoType::from_u16(ether_type_raw);
+
+        Ok(EthernetHeader {
+            dst_mac,
+            src_mac,
+            ether_type,
+        })
+    }
+}
 
 #[entry]
 fn efi_main() -> Status {
@@ -76,8 +139,31 @@ fn start_net_interface() -> Result {
             }
         };
 
+        let eth_header = match EthernetHeader::parse(&buffer[..header_size]) {
+            Ok(header) => header,
+            Err(err) => {
+                error!("Error parsing Ethernet header: {:?}", err);
+                continue;
+            }
+        };
+
+        let ether_type = eth_header.ether_type;
+
+        info!("We got a packet from {} protocol.", ether_type);
+        match ether_type {
+            EtherProtoType::ARP => {
+                ()
+            },
+            EtherProtoType::IPv4 | EtherProtoType::IPv6 | EtherProtoType::Unknown(_) => {
+                warn!("Ignore the packet, not implemented");
+                continue;
+            }
+        }
+
         info!("Received packet: {:?}", &buffer[..packet_size]);
         info!("Header size: {}", header_size);
+
+        /* TODO: parse ARP */
     }
 
     info!("Shutting down the network...");
