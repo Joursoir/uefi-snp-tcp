87bbe3923f01fc1a57e98622832aaa2949de74b9
Joursoir <chat@joursoir.net>
2024-10-15 02:24:14 +0300

    main: split up start/stop network interface into separate functions

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern crate alloc;
 use alloc::vec;
 use alloc::vec::Vec;
 use log::{error, warn, info};
-use uefi::boot::{self, SearchType};
+use uefi::boot::{self, ScopedProtocol, SearchType};
 use uefi::prelude::*;
 use uefi::proto::network::snp::{SimpleNetwork, NetworkMode, NetworkState};
 use uefi::{Identify, Result};
@@ -23,54 +23,15 @@ mod link;
 fn efi_main() -> Status {
     uefi::helpers::init().unwrap(); // Initialize uefi::helpers
 
-    start_net_interface().unwrap();
+    let snp = start_net_interface().unwrap();
+    let retval = packet_processing_loop(&snp);
+    stop_net_interface(snp).unwrap();
 
-    Status::SUCCESS
+    retval.status()
 }
 
-fn start_net_interface() -> Result {
-    info!("Looking for Simple Network device...");
-    let snp_handle =
-        *boot::locate_handle_buffer(SearchType::ByProtocol(&SimpleNetwork::GUID))?
-        .first()
-        .expect("Simple Network Protocol is missing");
-
-    // When we exclusive opens SNP protocol, the UEFI will remove any drivers 
-    // that opened SNP with BY_DRIVER by calling the driver's Stop() function. In UEFI
-    // network stack, MNP driver will open SNP 'BY_DRIVER'. So the code below exclusive
-    // opens SNP, MNP will uninstall itself and the whole UEFI network stack is
-    // disconnected except SNP and UNDI.
-    let snp = boot::open_protocol_exclusive::<SimpleNetwork>(
-        snp_handle,
-    )?;
-
+fn packet_processing_loop(snp: &ScopedProtocol<SimpleNetwork>) -> Result {
     let network_mode: &NetworkMode = snp.mode();
-
-    // When an EFI_SIMPLE_NETWORK_PROTOCOL driver initializes a
-    // network interface, the network interface is left in the EfiSimpleNetworkStopped state.
-    match network_mode.state {
-        NetworkState::STOPPED => {
-            info!("Network interface is currently stopped.");
-        }
-        NetworkState::STARTED => {
-            warn!("Network interface is started but not yet initialized.");
-            return Ok(())
-        }
-        NetworkState::INITIALIZED => {
-            warn!("Network interface is fully initialized.");
-            return Ok(())
-        }
-        _ => {
-            error!("Invalid network state");
-            return Err(Status::UNSUPPORTED.into());
-        }
-    }
-
-    info!("Proceeding with network setup...");
-    // Change the state of a network from “Stopped” to “Started”
-    snp.start().unwrap();
-    // Reset a network adapter and allocate the transmit and receive buffers
-    snp.initialize(1024, 1024).unwrap();
 
     info!("Waiting for packets...");
     loop {
@@ -150,11 +111,60 @@ fn start_net_interface() -> Result {
 
         /* TODO: parse ARP */
     }
+}
 
+fn start_net_interface() -> Result<ScopedProtocol<SimpleNetwork>> {
+    info!("Looking for Simple Network device...");
+    let snp_handle =
+        *boot::locate_handle_buffer(SearchType::ByProtocol(&SimpleNetwork::GUID))?
+        .first()
+        .expect("Simple Network Protocol is missing");
+
+    // When we exclusive opens SNP protocol, the UEFI will remove any drivers 
+    // that opened SNP with BY_DRIVER by calling the driver's Stop() function. In UEFI
+    // network stack, MNP driver will open SNP 'BY_DRIVER'. So the code below exclusive
+    // opens SNP, MNP will uninstall itself and the whole UEFI network stack is
+    // disconnected except SNP and UNDI.
+    let snp = boot::open_protocol_exclusive::<SimpleNetwork>(
+        snp_handle,
+    )?;
+
+    let network_mode: &NetworkMode = snp.mode();
+
+    // When an EFI_SIMPLE_NETWORK_PROTOCOL driver initializes a
+    // network interface, the network interface is left in the EfiSimpleNetworkStopped state.
+    match network_mode.state {
+        NetworkState::STOPPED => {
+            info!("Network interface is currently stopped.");
+        }
+        NetworkState::STARTED => {
+            warn!("Network interface is started but not yet initialized.");
+            return Err(Status::UNSUPPORTED.into());
+        }
+        NetworkState::INITIALIZED => {
+            warn!("Network interface is fully initialized.");
+            return Err(Status::UNSUPPORTED.into());
+        }
+        _ => {
+            error!("Invalid network state");
+            return Err(Status::UNSUPPORTED.into());
+        }
+    }
+
+    info!("Proceeding with network setup...");
+    // Change the state of a network from “Stopped” to “Started”
+    snp.start().unwrap();
+    // Reset a network adapter and allocate the transmit and receive buffers
+    snp.initialize(1024, 1024).unwrap();
+    Ok(snp)
+}
+
+fn stop_net_interface(snp: ScopedProtocol<SimpleNetwork>) -> Result {
     info!("Shutting down the network...");
     // Reset a network adapter, leaving it in a state that is safe for another driver to initialize
     snp.shutdown().unwrap();
     // Change the state of a network interface from “Started” to “Stopped”.
     snp.stop().unwrap();
+
     Ok(())
 }
