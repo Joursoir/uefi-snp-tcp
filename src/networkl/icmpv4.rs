8ad255a1084c2371cd111bc27b2369d185d6a968
Joursoir <chat@joursoir.net>
2024-10-20 23:04:30 +0300

    networkl: add ICMPv4 packet parser

---
+++ src/networkl/icmpv4.rs
@@ -0,0 +1,62 @@
+use uefi::prelude::*;
+use uefi::{Result};
+
+pub const NET_ICMPV4_HEADER_LENGTH: usize = 8;
+pub const NET_ICMPV4_TYPE_ECHO_REQ: u8 = 8;
+pub const NET_ICMPV4_TYPE_ECHO_REP: u8 = 0;
+
+pub struct Icmpv4Writer<'a> {
+    pub buffer: &'a mut [u8],
+}
+
+impl<'a> Icmpv4Writer<'a> {
+    pub fn new(buffer: &'a mut [u8]) -> Result<Self> {
+        if buffer.len() < NET_ICMPV4_HEADER_LENGTH {
+            return Err(Status::INVALID_PARAMETER.into());
+        }
+
+        Ok(Self { buffer })
+    }
+
+    pub fn set_type(&mut self, icmp_type: u8) {
+        self.buffer[0] = icmp_type;
+    }
+
+    pub fn set_code(&mut self, code: u8) {
+        self.buffer[1] = code;
+    }
+}
+
+pub struct Icmpv4Reader<'a> {
+    pub buffer: &'a [u8],
+}
+
+impl<'a> Icmpv4Reader<'a> {
+    pub fn new(buffer: &'a [u8]) -> Result<Self> {
+        if buffer.len() < NET_ICMPV4_HEADER_LENGTH {
+            return Err(Status::INVALID_PARAMETER.into());
+        }
+
+        Ok(Self { buffer })
+    }
+
+    pub fn r#type(&self) -> u8 {
+        self.buffer[0]
+    }
+
+    pub fn code(&self) -> u8 {
+        self.buffer[1]
+    }
+
+    pub fn checksum(&self) -> u16 {
+        ((self.buffer[2] as u16) << 8) | (self.buffer[3] as u16)
+    }
+
+    pub fn is_echo_request(&self) -> bool {
+        self.r#type() == NET_ICMPV4_TYPE_ECHO_REQ
+    }
+
+    pub fn payload(&self) -> &[u8] {
+        &self.buffer[NET_ICMPV4_HEADER_LENGTH..]
+    }
+}
